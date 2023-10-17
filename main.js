82edb60d164639f3763eec4c81faeb5faff24c77
Chuk <chukmunnlee@gmail.com>
2023-10-17 11:38:58 +0800

    added index.html

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const PORT = parseInt(process.env.PORT) || 3000
 // Create an instance of express
 const app = express()
 
+// Serve files in the static directory
+app.use(express.static(__dirname + '/static'))
+
 app.listen(PORT, () => {
    console.info(`Application started on port ${PORT} at ${new Date()}`)
 })
